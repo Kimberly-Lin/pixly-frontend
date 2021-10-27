3c4cb405373a048e51ff1c8266e7a5ca3736a98f
do-rh <87504110+do-rh@users.noreply.github.com>
2021-10-27 11:33:19 -0700

    added basic form for file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UploadImgForm from './UploadImgForm';
+import axios from 'axios';
+import { useState } from 'react';
+
+const API_URL='http://localhost:5000/upload';
 
 function App() {
+
+  const [imageFile, setImageFile] = useState(null);
+
+  async function handleImgUpload(image, caption) {
+    // Create an object of formData
+    setImageFile(image);
+    
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append(
+      'file',
+      image,
+      image.name
+    );
+    console.log(formData['file']);
+    // Request made to the backend api
+    // Send formData object
+    const resp = await axios.post(API_URL, formData);
+    // const resp = await axios.post(API_URL, image);
+    console.log(resp);
+  }
+
+  console.log('imageFile is:', imageFile);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UploadImgForm handleImgUpload={handleImgUpload} />
   );
 }
 
---
+++ src/UploadImgForm.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+/**Renders a form to upload an image and a caption
+ * 
+ * Props: handleImgUpload fn
+ * State: caption, selectedFile
+ * 
+ * App -> UploadImgForm
+ */
+function UploadImgForm({ handleImgUpload }) {
+    const [caption, setCaption] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        console.log('handle submit is running in form')
+        handleImgUpload(selectedFile, caption);
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={caption}
+                    onChange={(e) => setCaption(e.target.value)}
+                />
+
+                <input
+                    type="file"
+                    onChange={(e) => setSelectedFile(e.target.files[0])}
+                />
+                <button>
+                    Upload!
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default UploadImgForm;
